3e4ae3a84397b43e457759123bedb71d295af548
hossein ghanbari <41850112+hossein-ghanbari@users.noreply.github.com>
2021-09-21 17:02:56 +0430

    Update arduino_code.ino

--- arduino_code.ino
+++ arduino_code.ino
@@ -1,8 +1,6 @@
 //Declare the arduino pins
-int rm1 = 5;
-int rm2 = 6;
-int lm1 = 10;
-int lm2 = 9;
+int rm = 5;
+int lm = 10;
 int horn = 11;
 int light = 12;
 
@@ -20,10 +18,8 @@ double Thermistor(int RawADC) {
 void setup()
 {
   //initlize the mode of the pins
-  pinMode(lm1, OUTPUT);
-  pinMode(lm2, OUTPUT);
-  pinMode(rm1, OUTPUT);
-  pinMode(rm2, OUTPUT);
+  pinMode(lm, OUTPUT);
+  pinMode(rm, OUTPUT);
   pinMode(horn, OUTPUT);
   pinMode(light, OUTPUT);
 
@@ -45,40 +41,32 @@ void loop()
   if (val == 'FORWARD')
   {
     Serial.println("{status:'FORWARD'}");
-    digitalWrite(lm1, HIGH);
-    digitalWrite(rm1, HIGH);
-    digitalWrite(lm2, LOW);
-    digitalWrite(rm2, LOW);
+    digitalWrite(lm, HIGH);
+    digitalWrite(rm, HIGH);
   }
 
   //Backward
   if (val == 'BACKWARD')
   {
     Serial.println("{status:'BACKWARD'}");
-    digitalWrite(lm2, HIGH);
-    digitalWrite(rm2, HIGH);
-    digitalWrite(lm1, LOW);
-    digitalWrite(rm1, LOW);
+    digitalWrite(lm, LOW);
+    digitalWrite(rm, LOW);
   }
 
   //Right
   if (val == 'RIGHT')
   {
     Serial.println("{status:'RIGHT'}");
-    digitalWrite(lm1, HIGH);
-    digitalWrite(rm2, HIGH);
-    digitalWrite(lm2, LOW);
-    digitalWrite(rm1, LOW);
+    digitalWrite(lm, HIGH);
+    digitalWrite(rm, LOW);
   }
 
   //Left
   if (val == 'LEFT')
   {
     Serial.println("{status:'LEFT'}");
-    digitalWrite(lm2, HIGH);
-    digitalWrite(rm1, HIGH);
-    digitalWrite(lm1, LOW);
-    digitalWrite(rm2, LOW);
+    digitalWrite(rm, HIGH);
+    digitalWrite(lm, LOW);
   }
 
   //Horn
@@ -99,10 +87,8 @@ void loop()
   if (val == 'CLEAR')
   {
     Serial.println("{status:'CLEAR'}");
-    digitalWrite(lm1, LOW);
-    digitalWrite(rm1, LOW);
-    digitalWrite(lm2, LOW);
-    digitalWrite(rm2, LOW);
+    digitalWrite(lm, LOW);
+    digitalWrite(rm, LOW);
     digitalWrite(horn, LOW);
     digitalWrite(light, LOW);
   }
